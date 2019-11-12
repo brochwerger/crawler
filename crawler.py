f9b1ad7f15121cb74819246078868f30c0a4a6ae
Benny Rochwerger <bennyr@radware.com>
2019-11-12 20:38:25 +0200

    Added use of queues. Added option of passing a file (with a list of urls) or a single root url

--- crawler.py
+++ crawler.py
@@ -3,9 +3,22 @@ import queue
 
 from urlhandler import URLHandler
 
+usage = "USAGE: Required parameter: -f <filename> OR -u <URL>"
+
 def crawler(args):
-    print("Hello")
-    u = URLHandler(args.filename, None, None)
+
+    urlqueue = queue.Queue()
+    emailqueue = queue.Queue()
+
+    if args.url:
+        urlqueue.put([args.url, 1])
+
+    if args.filename:
+        with open(args.filename) as file:
+            for url in file:
+                urlqueue.put([url, 1])
+
+    u = URLHandler(urlqueue, emailqueue, args.maxdepth if args.maxdepth else -1)
     u.start()
 
     u.join()
@@ -14,6 +27,12 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--nthreads", type=int, default=10)
-    parser.add_argument("-f", "--filename", required=True)
+    parser.add_argument("--maxdepth", type=int)
+    parser.add_argument("-f", "--filename") #, required=True)
+    parser.add_argument("-u", "--url") #, required=True)
     args = parser.parse_args()
+    if args.filename == None and args.url == None:
+        print(usage)
+        exit()
+
     crawler(args)
