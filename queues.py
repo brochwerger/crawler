8503098f1fa5e2fa8eba32a648717d810ffcf93c
Benny Rochwerger <bennyr@radware.com>
2019-11-21 13:55:23 +0200

    - Added parameter to limit size of queue

--- queues.py
+++ queues.py
@@ -1,11 +1,12 @@
 import queue
 
 from abc import ABC, abstractmethod
+from sys import maxsize
 
 class BasicQueue(ABC):
 
-    def __init__(self):
-        self.q = queue.Queue()
+    def __init__(self, maxsize=0):
+        self.q = queue.Queue(maxisize=maxsize)
 
     def put(self, value):
         self.q.put(value)
